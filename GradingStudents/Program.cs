c2a844cf7df7b1fbb1e8f2018fdbb859c232952e
Furkan yagci <furkanyagcify@gmail.com>
2022-01-19 14:58:14 +0300

    .

---
+++ GradingStudents/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System;
+
+class Result
+{
+
+    public static List<int> gradingStudents(List<int> grades)
+    {
+        for (int i = 0; i < grades.Count; i++)
+        {
+            if (grades[i] >= 38)
+            {
+                var currDiff = 5 - (grades[i] % 5);
+
+                if (currDiff < 3)
+                {
+                    grades[i] += currDiff;
+                }
+
+            }
+        }
+
+        return grades;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Kaç adet not girilecek : ");
+        int gradesCount = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> grades = new List<int>();
+
+        for (int i = 0; i < gradesCount; i++)
+        {
+            Console.WriteLine("Not giriniz : ");
+            int gradesItem = Convert.ToInt32(Console.ReadLine().Trim());
+            grades.Add(gradesItem);
+        }
+
+        List<int> result = Result.gradingStudents(grades);
+
+        Console.WriteLine("Notların yuvarlanmış halleri");
+        foreach (var item in result)
+        {
+            Console.WriteLine(item.ToString());
+        }
+    }
+}
