1fd6eb0607b2db95d3e4799e02916eb726b86033
Furkan yagci <furkanyagcify@gmail.com>
2022-01-19 14:38:26 +0300

    .

---
+++ SalesbyMatch/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'sockMerchant' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER n
+     *  2. INTEGER_ARRAY ar
+     */
+
+    public static int sockMerchant(int n, List<int> ar)
+    {
+        var sockCount = 0;
+        var set = new HashSet<int>();
+        foreach (var i in ar)
+        {
+            if (!set.Contains(i))
+            {
+                set.Add(i);
+            }
+            else
+            {
+                sockCount++;
+                set.Remove(i);
+            }
+        }
+
+        return sockCount;
+
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Kaç Çorap var :");
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+        Console.WriteLine("Çorap tiplerini giriniz Örn: 10, 10, 20, 30, 50, 20, 10, 20, 10");
+        List<int> ar = new List<int>();
+        for (int i = 0; i < n; i++)
+        {
+            ar.Add(Convert.ToInt32(Console.ReadLine()));
+        }
+
+        int result = Result.sockMerchant(n, ar);
+
+        Console.WriteLine("Çift çorap sayısı : "+result);
+    }
+}
